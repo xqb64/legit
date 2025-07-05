a40ac546367329167ac25147fbba1e2aff36a778
xqb64 <x@xqb.ch>
2025-07-05 07:05:02 +0200

    normalize the common_ancestors test suite

--- tests/test_ancestors.py
+++ tests/test_ancestors.py
@@ -1,19 +1,12 @@
-import shutil
 import datetime
 import pytest
-from pathlib import Path
 
-# Import your modules
 from legit.database import Database
 from legit.author import Author
 from legit.commit import Commit
 from legit.common_ancestors import CommonAncestors
 from legit.bases import Bases
 
-
-@pytest.fixture
-def db(repo_path):
-    return Database(repo_path / "objects")
 
 class GraphBuilder:
     def __init__(self, db):
@@ -32,51 +25,72 @@ class GraphBuilder:
             parents = [] if parent is None else [parent]
             self.commit(parents, msg)
 
+
+@pytest.fixture
+def db(repo_path):
+    return Database(repo_path / "objects")
+
+
 @pytest.fixture
 def builder(db):
     return GraphBuilder(db)
+
 
 @pytest.fixture
 def ancestor(builder, db):
     def _ancestor(left, right):
         common = CommonAncestors(db, builder.commits[left], [builder.commits[right]])
-        oids = common.find()
-        msgs = [db.load(oid).message for oid in oids]
+        msgs = [db.load(oid).message for oid in common.find()]
         return msgs[0] if len(msgs) == 1 else sorted(msgs)
     return _ancestor
+
 
 @pytest.fixture
 def merge_base(builder, db):
     def _merge_base(left, right):
         bases = Bases(db, builder.commits[left], builder.commits[right])
-        oids = bases.find()
-        msgs = [db.load(oid).message for oid in oids]
+        msgs = [db.load(oid).message for oid in bases.find()]
         return msgs[0] if len(msgs) == 1 else sorted(msgs)
     return _merge_base
 
-# Linear history tests
+
 class TestLinearHistory:
+    
+    #   o---o---o---o
+    #   A   B   C   D
+
     @pytest.fixture(autouse=True)
     def setup_chain(self, builder):
         builder.chain([None, "A", "B", "C", "D"])
 
-    def test_commit_with_itself(self, ancestor):
+    def test_it_finds_the_common_ancestor_of_a_commit_with_itself(self, ancestor):
         assert ancestor("D", "D") == "D"
 
-    def test_ancestor_of_other(self, ancestor):
+    def test_it_finds_the_commit_that_is_an_ancestor_of_other(self, ancestor):
         assert ancestor("B", "D") == "B"
 
-    def test_reversed_args(self, ancestor):
+    def test_it_finds_the_same_commit_if_the_args_are_reversed(self, ancestor):
         assert ancestor("D", "B") == "B"
 
-    def test_root_commit(self, ancestor):
+    def test_it_finds_a_root_commit(self, ancestor):
         assert ancestor("A", "C") == "A"
 
-    def test_root_with_itself(self, ancestor):
+    def test_it_finds_the_intersection_of_the_root_commit_with_itself(self, ancestor):
         assert ancestor("A", "A") == "A"
 
-# Forking history tests
+
 class TestForkingHistory:
+
+    #          E   F   G   H
+    #          o---o---o---o
+    #         /         \
+    #        /  C   D    \
+    #   o---o---o---o     o---o
+    #   A   B    \        J   K
+    #             \
+    #              o---o---o
+    #              L   M   N
+
     @pytest.fixture(autouse=True)
     def setup_chain(self, builder):
         b = builder
@@ -85,25 +99,32 @@ class TestForkingHistory:
         b.chain(["G", "J", "K"])
         b.chain(["C", "L", "M", "N"])
 
-    def test_nearest_fork_point(self, ancestor):
+    def test_it_finds_the_nearest_fork_point(self, ancestor):
         assert ancestor("H", "K") == "G"
 
-    def test_multiple_forks_away(self, ancestor):
+    def test_it_finds_an_ancestor_multiple_forks_away(self, ancestor):
         assert ancestor("D", "K") == "B"
 
-    def test_same_fork_any_point(self, ancestor):
+    def test_it_finds_the_same_fork_point_for_any_point_on_a_branch(self, ancestor):
         assert ancestor("D", "L") == "C"
         assert ancestor("M", "D") == "C"
         assert ancestor("D", "N") == "C"
 
-    def test_ancestor_of_other(self, ancestor):
+    def test_it_finds_the_commit_that_is_an_ancestor_of_other(self, ancestor):
         assert ancestor("K", "E") == "E"
 
-    def test_root_commit(self, ancestor):
+    def test_it_finds_the_root_commit(self, ancestor):
         assert ancestor("J", "A") == "A"
 
-# Merge tests
+
 class TestWithMerge:
+
+    #   A   B   C   G   H
+    #   o---o---o---o---o
+    #        \     /
+    #         o---o---o
+    #         D   E   F
+
     @pytest.fixture(autouse=True)
     def setup_chain(self, builder):
         b = builder
@@ -112,15 +133,22 @@ class TestWithMerge:
         b.commit(["C", "E"], "G")
         b.chain(["G", "H"])
 
-    def test_most_recent_common_ancestor(self, ancestor):
+    def test_it_finds_the_most_recent_common_ancestor(self, ancestor):
         assert ancestor("H", "F") == "E"
 
-    def test_common_ancestor_merge_parents(self, ancestor):
+    def test_it_finds_the_common_ancestor_of_a_merge_and_its_parents(self, ancestor):
         assert ancestor("C", "G") == "C"
         assert ancestor("G", "E") == "E"
 
-# Merge further from one parent
+
 class TestMergeFurtherFromOneParent:
+
+    #   A   B   C   G   H   J
+    #   o---o---o---o---o---o
+    #        \     /
+    #         o---o---o
+    #         D   E   F
+
     @pytest.fixture(autouse=True)
     def setup_chain(self, builder):
         b = builder
@@ -129,14 +157,22 @@ class TestMergeFurtherFromOneParent:
         b.commit(["C", "E"], "G")
         b.chain(["G", "H", "J"])
 
-    def test_all_common_ancestors(self, ancestor):
+    def test_it_finds_all_the_common_ancestors(self, ancestor):
         assert ancestor("J", "F") == ["B", "E"]
 
-    def test_best_common_ancestor(self, merge_base):
+    def test_it_finds_the_best_common_ancestor(self, merge_base):
         assert merge_base("J", "F") == "E"
 
-# Commits between common ancestor and merge
+
 class TestCommitsBetweenAncestorAndMerge:
+
+    #   A   B   C       H   J
+    #   o---o---o-------o---o
+    #        \         /
+    #         o---o---o G
+    #         D  E \
+    #               o F
+
     @pytest.fixture(autouse=True)
     def setup_chain(self, builder):
         b = builder
@@ -146,14 +182,25 @@ class TestCommitsBetweenAncestorAndMerge:
         b.commit(["C", "G"], "H")
         b.chain(["H", "J"])
 
-    def test_all_common_ancestors(self, ancestor):
+    def test_it_finds_all_the_common_ancestors(self, ancestor):
         assert ancestor("J", "F") == ["B", "E"]
 
-    def test_best_common_ancestor(self, merge_base):
+    def test_it_finds_the_best_common_ancestor(self, merge_base):
         assert merge_base("J", "F") == "E"
 
-# Stale results history
+
 class TestStaleResultsHistory:
+
+    #   A   B   C             H   J
+    #   o---o---o-------------o---o
+    #        \      E        /
+    #         o-----o-------o
+    #        D \     \     / G
+    #           \     o   /
+    #            \    F  /
+    #             o-----o
+    #             P     Q
+
     @pytest.fixture(autouse=True)
     def setup_chain(self, builder):
         b = builder
@@ -164,19 +211,32 @@ class TestStaleResultsHistory:
         b.commit(["C", "G"], "H")
         b.chain(["H", "J"])
 
-    def test_best_common_ancestor_bidirectional(self, ancestor):
+    def test_it_finds_the_best_common_ancestor(self, ancestor):
         assert ancestor("J", "F") == "E"
         assert ancestor("F", "J") == "E"
 
-# Many common ancestors
+
 class TestManyCommonAncestors:
+    
+    #         L   M   N   P   Q   R   S   T
+    #         o---o---o---o---o---o---o---o
+    #        /       /       /       /
+    #   o---o---o...o---o...o---o---o---o---o
+    #   A   B  C \  D  E \  F  G \  H   J   K
+    #             \       \       \
+    #              o---o---o---o---o---o
+    #              U   V   W   X   Y   Z
+
+
     @pytest.fixture(autouse=True)
     def setup_chain(self, builder):
         b = builder
-        pads1 = [f"pad-1-{i}" for i in range(1,5)]
-        pads2 = [f"pad-2-{i}" for i in range(1,5)]
+        pads1 = [f"pad-1-{i}" for i in range(1, 5)]
+        pads2 = [f"pad-2-{i}" for i in range(1, 5)]
         backbone = [None, "A", "B", "C"] + pads1 + ["D", "E"] + pads2 + ["F", "G"] + ["H", "J", "K"]
+
         b.chain(backbone)
+        
         b.chain(["B", "L", "M"])
         b.commit(["M", "D"], "N")
         b.chain(["N", "P"])
@@ -184,15 +244,16 @@ class TestManyCommonAncestors:
         b.chain(["Q", "R"])
         b.commit(["R", "H"], "S")
         b.chain(["S", "T"])
+        
         b.chain(["C", "U", "V"])
         b.commit(["V", "E"], "W")
         b.chain(["W", "X"])
         b.commit(["X", "G"], "Y")
         b.chain(["Y", "Z"])
 
-    def test_multiple_candidates(self, ancestor):
+    def test_it_finds_multiple_candidate_common_ancestors(self, ancestor):
         assert ancestor("T", "Z") == ["B", "D", "G"]
 
-    def test_best_common_ancestor(self, merge_base):
+    def test_it_finds_the_best_common_ancestor(self, merge_base):
         assert merge_base("T", "Z") == "G"
 
